fd29ef0f9764795305e4b6d40895f7bd81759a7d
Dendi Handian <handian.dendi@gmail.com>
2018-09-14 21:26:36 +0700

    more example routes

--- routes/api/example.js
+++ routes/api/example.js
@@ -1,10 +1,43 @@
 const exampleRoutes = function (app) {
-  app.get('/api/example', function (req, res, next) {
-    res.status(200).json({
-      message: 'Example List',
-      data: {}
+  app.route('/api/examples')
+    .get( function (req, res) {
+      res.status(200).json({
+        message: 'Example List',
+        data: {}
+      })
     })
-  })
+    .post( function (req, res) {
+      res.status(201).json({
+        message: 'Example Created',
+        data: {}
+      })
+    })
+
+  app.route('/api/examples/:id')
+    .get( function (req, res) {
+      res.status(200).json({
+        message: 'Example Detail',
+        data: {
+          id: parseInt(req.params.id)
+        }
+      })
+    })
+    .patch( function (req, res) {
+      res.status(200).json({
+        message: 'Example Updated',
+        data: {
+          id: parseInt(req.params.id)
+        }
+      })
+    })
+    .delete( function (req, res) {
+      res.status(200).json({
+        message: 'Example Deleted',
+        data: {
+          id: parseInt(req.params.id)
+        }
+      })
+    })
 }
 
 module.exports = exampleRoutes
