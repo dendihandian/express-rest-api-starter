99b6f27345c1a77e5f90155d8ec50f74792f5879
Dendi Handian <handian.dendi@gmail.com>
2018-09-20 22:48:54 +0700

    add cors

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,12 @@
 // import and define express app instance
 const app = require('express')()
 
+// import and define cors instance
+const cors = require('cors')
+
+// enable cors
+app.use(cors())
+
 // import and apply router
 require('../routes/')(app)
 
